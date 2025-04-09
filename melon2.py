e37ec25b585d96f02aedaa4beb81e61e4886856c
hanseol0921 <leenayoungmzzu012@gmail.com>
2025-04-09 17:32:42 +0000

    탑50/탑10/가수검색

--- melon2.py
+++ melon2.py
@@ -4,11 +4,11 @@ import random
 import time
 
 print("==================")
-print("1. 멜론 top 100")
-print("2. 멜론 top 50")
-print("3. 멜론 top 10")
+print("1. 멜론 TOP100")
+print("2. 멜론 TOP50")
+print("3. 멜론 TOP10")
 print("4. AI 추천 노래")
-print("5. 가수 이름 검색")
+print("5. TOP100 가수 검색")
 print("==================")
 # 멜론 차트 주소
 url = "https://www.melon.com/chart/index.htm"
@@ -48,39 +48,52 @@ n = input("메뉴선택: ")
 # 멜론 top 100 출력
 # n = int(n) / n은 정수, 연산을 해야한다면
 if n == "1":
-    print("멜론 TOP100")
+    print("[멜론 TOP100]")
     # 4. 추출 결과 출력
     for i in range(100):
         print(f"{songs[i][0]}. {songs[i][1]} - {songs[i][2]}")
 
-
-
 # 메뉴선택(입력) : 2
 # 만약 2을 입력하면
 # 멜론 top 50 출력
+
 elif n == "2":
-    print("멜론 TOP50")
+    print("[멜론 TOP50]")
+    for i in range(50):
+        print(f"{songs[i][0]}. {songs[i][1]} - {songs[i][2]}")
 
 elif n == "3":
-    print("멜론 TOP10")
+    print("[멜론 TOP10]")
+    for i in range(10):
+        print(f"{songs[i][0]}. {songs[i][1]} - {songs[i][2]}")
 
 elif n == "4":
-    print("AI 노래 추천")
+    print("[AI 노래 추천]")
     
     melon = random.choice(songs)
     random_rank, random_title, random_artist = melon
     print("오늘 멜론 Top 100 추천곡은")
 
     print(f"{random_rank}위 '{random_artist} - {random_title}' 입니다.") 
+
 # 메뉴선택(입력) : 5
 # 만약 5을 입력하면
 # 가수 이름 검색
-elif n == "5":
-    print("가수 검색")
-    artist = input("가수를 입력하세요: ")
-
 # 5를 입력하면 가수이름을 입력할 입력창 필요
 # 가수 입력 시 해당 가수 노래 리스트 출력
 
+elif n == "5":
+    print("[TOP100 가수 검색]")
+    a = input("가수를 입력하세요: ")
+    num = 0
+    for i in range(100):
+        if a == songs[i][2]:
+            print(f"{songs[i][0]}. {songs[i][1]} - {songs[i][2]}")
+            num = num+1
+    if num == 0:
+        print("입력하신 가수가 TOP100에 없습니다")
+
+
+
 else:
-    print("1~5까지 값을 입력하세요")
+    print("1~5까지의 값을 입력하세요")
