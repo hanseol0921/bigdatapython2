4178a7a83c1d31a4b6a5b2b9fe2d167fcf17c2d3
hanseol0921 <leenayoungmzzu012@gmail.com>
2025-04-20 09:22:49 +0000

    파일저장하기

--- func.py
+++ func.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import time
+import csv
 
 def m000(a, c):
     print(a)
@@ -118,13 +119,11 @@ def m10(a):
 
     songs = soup.select('tr[data-song-no]')
 
-    for index, song in enumerate(songs):
-        if index >= 10:
-            break
+    for song in songs:
         rank = song.select_one('span.rank').text.strip()
         title = song.select_one('div.ellipsis.rank01 a').text.strip()
         artist = song.select_one('div.ellipsis.rank02 a').text.strip()
-        print(f'{rank}위 {title} - {artist}')
+        song_list.append((rank, title, artist))
 
 def m_artist(e):
     print(e)
@@ -136,7 +135,7 @@ def m_artist(e):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
-
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         songs = soup.select('tr[data-song-no]')
@@ -157,3 +156,5 @@ def m_artist(e):
             print(f"[TOP 100곡 내 <{s}>의 노래가 없어요.]")
     else:
         print(f'[웹 페이지를 가져오는 데 실패했어요. T.T | 상태 코드: {response.status_code}]')
+
+def 
--- m_main.py
+++ m_main.py
@@ -21,8 +21,10 @@ elif n == "3":
 elif n == "4":
     func.m_random("[노래추천기능]")
 elif n == "5":
-    func.m_artist("TOP100 가수 검색")
+    func.m_artist("[TOP100 가수 검색]")
 elif n == "6":
+    func.melon_csv("[멜론 TOP100을 파일에 저장합니다.]")
+
 
 else:
     print("1~5까지의 수를 입력해 주세요")
